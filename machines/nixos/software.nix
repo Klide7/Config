f3bad23c093058ea03f24f6ff66baad2e9243685
Leon Schumacher <leonsch@protonmail.com>
2024-04-27 15:39:54 +0200

    prismlauncher: add important java versions

--- machines/nixos/software.nix
+++ machines/nixos/software.nix
@@ -1,6 +1,10 @@
 { pkgs, ... }: {
   # pakete finden auf https://mynixos.com
   home-manager.users.klide.home.packages = with pkgs; [
+    # minecraft stuff
     prismlauncher
+    openjdk8
+    openjdk17
+    openjdk22
   ];
 }
